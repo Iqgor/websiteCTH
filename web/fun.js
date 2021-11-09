c9e217722d286c1b1110cc973017a2cba0388f95
Igor <33993@ma-web.nl>
2021-11-09 12:25:22 +0100

    Les 13 update

---
+++ web/fun.js
@@ -0,0 +1,5 @@
+function veranderachtergrond() {
+    document.getElementById("intro").style.background="orange"
+    document.getElementById("intro").style.border="black"
+    document.getElementsByClassName("Cheat1").style.border="black"
+}
